16c8921a784be39e6b9d7841a1bb26c72bebfc71
govind6498 <govindsingh6498@gmail.com>
2021-09-24 00:59:49 +0530

    Added:Display Welcome message

---
+++ src/com/bridgelabz/linkedlist/MyNode.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedlist;
+
+public class MyNode {
+	public static void main(String[] args) {
+		System.out.println("------Welcome to data strcutrue:Linked List------");
+	}
+}
